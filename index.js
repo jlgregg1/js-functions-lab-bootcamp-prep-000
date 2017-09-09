f255c7dfdba729c0b84a7765d51d515bbf21af9e
Jennifer Stewart <jlgregg1@gmail.com>
2017-09-09 06:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ function happyHolidays () {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!``
+  return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return `Happy Independence Day, ${name}!``
+  return `Happy Independence Day, ${name}!`
 }
 function holidayCountdown(holiday, days) {
-  return `It's ${days} days until {holiday}!``
+  return `It's ${days} days until {holiday}!`
 }
